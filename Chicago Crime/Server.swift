fc0ebe69c43c6074e72065a96dc331554ec7481d
Ethan Gates <ethantakeshi@users.noreply.github.com>
2015-10-26 22:46:30 -0700

    Use 4 for tab width and indent because more can fit on the page that way oh also I added a swift thing yay!

---
+++ Chicago Crime/Server.swift
@@ -0,0 +1,20 @@
+//
+//  Server.swift
+//  Chicago Crime
+//
+//  Created by Ethan Gates on 10/26/15.
+//  Copyright © 2015 Kalvin Loc. All rights reserved.
+//
+
+import Foundation
+
+let API = NSURL(string:"https://data.cityofchicago.org/resource/ijzp-q8t2.json")
+
+
+class Server {
+        static let shared = Server();
+        func getstuff(){
+                
+        }
+        
+}
