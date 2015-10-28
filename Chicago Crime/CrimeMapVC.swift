c7fa3e81b34cc0a809920c5d3e06d5e0807ed601
Kalvin Loc <kalvin126@gmail.com>
2015-10-28 15:15:44 -0700

    Initial MapPoint plotting

--- Chicago Crime/CrimeMapVC.swift
+++ Chicago Crime/CrimeMapVC.swift
@@ -13,23 +13,35 @@ class CrimeMapVC: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
 
-    var crimes:Array<report> = Array()
+    var crimes:Array<Report> = Array()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // set initial mapView position
-        let center = CLLocationCoordinate2DMake(41.996101696641887, -87.950124889717827)
-        let span = MKCoordinateSpanMake(0.5, 1.0)
-        mapView.setRegion(MKCoordinateRegionMake(center, span), animated: false)
+        func assign(elements:Array<Report>) {
+            self.crimes = elements
 
-        // add annotation
-        //mapView.addAnnotation(<#T##annotation: MKAnnotation##MKAnnotation#>)
+            // add annotation
+            mapView.addAnnotations(crimes)
+            print("Plotted points")
+        }
+
+        Server.shared.getstuff(assign)
+
+        // set initial mapView position
+        let center = CLLocationCoordinate2DMake(41.8570092871228, -87.6975366951543)
+        let span = MKCoordinateSpanMake(0.4, 0.7)
+        mapView.setRegion(MKCoordinateRegionMake(center, span), animated: false)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func pressedbutton(sender: AnyObject) {
+        let region = mapView.region
+        let cencoord = mapView.centerCoordinate
+        print("region \(region)  \(cencoord)");
     }
 
 //    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
