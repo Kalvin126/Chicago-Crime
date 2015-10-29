1e3fe55996754e7fe8bcd16058b71d2fcf8db3b7
Ethan Gates <ethantakeshi@users.noreply.github.com>
2015-10-29 13:57:20 -0700

    two things - putting types into a separate file that just has types of filters - creating a filter class which will always be the type passed to the Server object

---
+++ Chicago Crime/Types.swift
@@ -0,0 +1,76 @@
+//
+//  Types.swift
+//  Chicago Crime
+//
+//  Created by Ethan Gates on 10/29/15.
+//  Copyright © 2015 Kalvin Loc. All rights reserved.
+//
+
+import Foundation
+
+
+
+let PrimaryTypes:Array<String> = ["ARSON",
+    "ASSAULT",
+    "BATTERY",
+    "BURGLARY",
+    "CRIMINAL DAMAGE",
+    "CRIMINAL SEXUAL ASSAULT",
+    "CRIMINAL TRESPASS",
+    "DECEPTIVE PRACTICE",
+    "HOMOCIDE",
+    "INTERFERENCE WITH PUBLIC OFFICER",
+    "INTIMIDATION",
+    "KIDNAPPING",
+    "NARCOTICS",
+    "NON - CRIMINAL",
+    "MOTOR VEHICLE THEFT",
+    "OFFENSE INVOLVING CHILDREN",
+    "OTHER OFFENSE",
+    "PROSTITUTION",
+    "PUBLIC PEACE VIOLATION",
+    "ROBBERY",
+    "SEX OFFENSE",
+    "THEFT",
+    "WEAPONS VIOLATION"]
+
+enum PrimaryType {
+    case ARSON
+    case BATTERY
+    case BURGLARY
+    case CRIM_DAMAGE
+    case CRIM_SEXUAL_ASSAULT
+    case CRIM_TRESPASS
+    case DECEPTIVE_PRACTICE
+    case HOMOCIDE
+    case INTERFERENCE_OFFICER
+    case INTIMIDATION
+    case KIDNAPPING
+    case NARCOTICS
+    case NON_CRIMINAL
+    case MOTOR_THEFT
+    case OFFENSE_CHILDREN
+    case OTHER_OFFENSE
+    case PROSTITUTION
+    case PUBLIC_PEACE_VIOLATION
+    case ROBBERY
+    case SEX_OFFENSE
+    case THEFT
+    case WEAPONS_VIOLATION
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
